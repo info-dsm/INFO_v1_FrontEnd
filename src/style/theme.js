a5c66d1d414544474943a01689d8c29fcb7ef547
jingeon27 <jingeon27@gmail.com>
2022-10-05 22:28:05 +0900

    색상추가

--- src/style/theme.js
+++ src/style/theme.js
@@ -5,11 +5,11 @@ export const theme = {
     blue: "#4000ff",
     green: "#61ff00",
     gray: "#868686",
-    whiteGray: "rgba(217, 217, 217, 0.01)",
-    angular: "conic-gradient(rgba(255, 255, 255, 1),rgba(114, 67, 255, 1))",
-    bluelinear:
-      "linear-gradient(rgba(114, 67, 255, 1), rgba(171, 145, 248, 1))",
-    whitelinear:
-      "linear-gradient(rgba(255, 255, 255, 1),rgba(245, 242, 255, 1))",
+    whiteGray: "F5F2FF",
+    mediumBlue: "7243FF",
+    purple: "AB91F8",
+    navy: "7243FF",
+    mediumSlateBlue: "AA90F8",
+    plum: "B59CFF",
   },
 };
