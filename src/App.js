86f0c3a5a2c0320b4fe7de31b1270f48faacf6c9
jingeon27 <jingeon27@gmail.com>
2022-10-12 08:26:32 +0900

    UPDATE :: theme 색상 추가 및 footer position 변경 및 redux provider 추가

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 /* eslint-disable no-restricted-globals */
 import { ThemeProvider, createGlobalStyle } from "styled-components";
 import { theme } from "./style/theme";
-import Header from "./components/common/header";
 import Footer from "./components/common/footer";
+import { Provider } from "react-redux";
+import { store } from "./redux";
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Header />
-      <Footer />
+      <Provider store={store}>
+        <GlobalStyle />
+        <Footer />
+      </Provider>
     </ThemeProvider>
   );
 }
 export default App;
 const GlobalStyle = createGlobalStyle`
-  user-select:none;
+  body,html {
+    user-select:none;
   -webkit-user-select:none;
   -moz-user-select:none;
   -ms-user-select:none;
+  }
+
+  * {
+    outline: none;
+    box-sizing: border-box;
+  }
 `;
--- src/components/common/header/index.jsx
+++ src/components/common/header/index.jsx
@@ -1,7 +1,8 @@
 import HeaderImage from "../../../images/Header.png";
 import LogoImage from "../../../images/Logo.png";
 import styled from "styled-components";
-const Header = () => {
+const Header = (props) => {
+  const { title, description } = props;
   const menu = [
     { menu: "회사" },
     { menu: "모집공고" },
@@ -12,37 +13,41 @@ const Header = () => {
   ];
   return (
     <>
-      <HeaderImg src={HeaderImage} alt="헤더이미지입니다." />
-      <LogoImg src={LogoImage} alt="로고이미지입니다." />
-      <MenuProps>
-        {menu.map((nav) => (
-          <>
-            <Menu>{nav.menu}</Menu>
-          </>
-        ))}
-      </MenuProps>
+      <HeaderImg>
+        <Nav>
+          <LogoImg src={LogoImage} alt="로고이미지입니다." />
+          <MenuProps>
+            {menu.map((nav) => (
+              <>
+                <Menu>{nav.menu}</Menu>
+              </>
+            ))}
+          </MenuProps>
+        </Nav>
+        <Title>
+          <h1>{title}</h1>
+          <span>{description}</span>
+        </Title>
+      </HeaderImg>
     </>
   );
 };
 export default Header;
-const HeaderImg = styled.img`
-  width: 100%;
+const HeaderImg = styled.div`
+  width: 100vw;
+  height: 400px;
+  background-image: url(${HeaderImage});
+  padding: 40px 300px;
 `;
 const LogoImg = styled.img`
-  position: absolute;
   width: 115px;
   height: 43px;
-  left: 284px;
-  top: 29px;
 `;
 const MenuProps = styled.ul`
-  position: absolute;
   width: 509px;
   height: 18px;
   display: flex;
   justify-content: space-between;
-  right: 300px;
-  top: 41px;
 `;
 const Menu = styled.li`
   font-family: "Nanum Gothic", sans-serif;
@@ -52,4 +57,20 @@ const Menu = styled.li`
   list-style: none;
   position: relative;
   color: ${(props) => props.theme.colors.white};
+`;
+const Nav = styled.div`
+  width: 100%;
+  display: inline-flex;
+  justify-content: space-between;
+`;
+const Title = styled.div`
+  margin-top: 67px;
+  color: #fff;
+  h1 {
+    margin: 0;
+    font-size: 64px;
+  }
+  span {
+    font-size: 24px;
+  }
 `;
