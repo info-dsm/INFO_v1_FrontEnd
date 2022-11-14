fe31e23347fa20c4047237bb6f3d1ee45a71e4ad
LeeDongHyeon <ldh7228@gmail.com>
2022-11-15 03:57:48 +0900

    UPDATE : Update Alert Design

--- src/components/common/notice/index.jsx
+++ src/components/common/notice/index.jsx
@@ -1,8 +1,6 @@
 import "./style.css";
 export const Notice = (props) => {
-  const asdf = document.getElementsByClassName("MainDiv");
-  console.log(asdf);
-  if (asdf.length === 0) {
+  if (document.getElementsByClassName("MainDiv").length === 0) {
     const body = document.querySelector("body");
     const mainDiv = document.createElement("div");
     const logoSpan = document.createElement("span");
@@ -10,33 +8,31 @@ export const Notice = (props) => {
     const messageDiv = document.createElement("div");
     const barDiv = document.createElement("div");
 
-    mainDiv.classList.add("MainDiv");
-    logoSpan.classList.add("Logo");
-    iconDiv.classList.add("Icon");
-    messageDiv.classList.add("Message");
+    mainDiv.classList.add("NoticeMainDiv");
+    logoSpan.classList.add("NoticeLogo");
+    iconDiv.classList.add("NoticeIcon");
+    messageDiv.classList.add("NoticeMessage");
     messageDiv.innerText = `${props.message}`;
-    barDiv.classList.add("Bar");
+    barDiv.classList.add("NoticeBar");
 
     if (props.state === "success") {
-      mainDiv.classList.add("SuccessMain");
-      iconDiv.classList.add("SuccessIcon");
-      barDiv.classList.add("SuccessBar");
+      mainDiv.classList.add("NoticeSuccessMain");
+      iconDiv.classList.add("NoticeSuccessIcon");
+      barDiv.classList.add("NoticeSuccessBar");
     } else if (props.state === "error") {
-      mainDiv.classList.add("ErrorMain");
-      barDiv.classList.add("ErrorBar");
+      mainDiv.classList.add("NoticeErrorMain");
+      barDiv.classList.add("NoticeErrorBar");
     }
 
     logoSpan.appendChild(iconDiv);
     mainDiv.appendChild(logoSpan);
     mainDiv.appendChild(messageDiv);
     mainDiv.appendChild(barDiv);
-    console.log(mainDiv);
 
     body.appendChild(mainDiv);
 
     setTimeout(() => {
       body.removeChild(mainDiv);
-      console.log(mainDiv);
     }, 3500);
     clearTimeout();
   }
