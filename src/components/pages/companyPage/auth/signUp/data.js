11c5db83a4fc8a444cf74eb0d1711478fcf9f90f
LeeDongHyeon <ldh7228@gmail.com>
2022-12-01 00:10:56 +0900

    ADD: Add Data

---
+++ src/components/pages/companyPage/auth/signUp/data.js
@@ -0,0 +1,72 @@
+export const fileName = [
+  { name: "businessRegisteredCertificate", kr: "사업자 등록증" },
+  { name: "companyIntroductionFile", kr: "회사 소개 파일" },
+  { name: "companyLogo", kr: "회사 로고" },
+  { name: "companyPhotoList", kr: "회사 사진 목록" },
+];
+
+export const contactorArr = [
+  {
+    title: "대표자",
+    state: "contactorName",
+    placeholder: "대표자를 입력해주세요",
+    type: "text",
+  },
+  {
+    title: "소속 부서",
+    state: "contactorRank",
+    placeholder: "소속 부서를 입력해주세요",
+    type: "text",
+  },
+  {
+    title: "전화번호",
+    state: "phoneNumber",
+    placeholder: "ex) 02 or 0xx-xxxx-xxxx",
+    type: "text",
+  },
+  {
+    title: "휴대전화",
+    state: "contactorPhone",
+    placeholder: "ex) 02 or 0xx-xxxx-xxxx",
+    type: "text",
+  },
+];
+
+export const companyArr = [
+  {
+    title: "회사 명",
+    state: "companyName",
+    placeholder: "회사 명을 입력해주세요",
+    type: "text",
+  },
+  {
+    title: "사업자\n등록번호",
+    state: "companyNumber",
+    placeholder: "ex) 000-00-00000",
+    type: "text",
+  },
+  {
+    title: "대표자",
+    state: "representative",
+    placeholder: "대표자를 입력해주세요",
+    type: "text",
+  },
+  {
+    title: "근로자 수",
+    state: "workerCount",
+    placeholder: "근로자 수를 입력해주세요",
+    type: "number",
+  },
+  {
+    title: "설립연도",
+    state: "establishedAt",
+    placeholder: "설립연도를 입력해주세요",
+    type: "number",
+  },
+  {
+    title: "연매출액",
+    state: "annualSales",
+    placeholder: "연매출액을 입력해주세요",
+    type: "number",
+  },
+];
