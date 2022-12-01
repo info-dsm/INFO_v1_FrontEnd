3ed38544137e34fd6ebc95171797d9585ad3bf85
LeeDongHyeon <ldh7228@gmail.com>
2022-12-02 02:28:51 +0900

    UPDATE: Update MainView

--- src/components/pages/studentPage/Main/index.jsx
+++ src/components/pages/studentPage/Main/index.jsx
@@ -10,15 +10,20 @@ import Header from "../../../common/header/index.jsx";
 import { useQuery } from "@tanstack/react-query";
 import LoadingPage from "../../../common/loading";
 import ErrorPage from "../../../common/error";
+import { useNavigate } from "react-router-dom";
+import { initialCompany, initialNotice } from "../../../../export/data.js";
+import NoticeModal from "../NoticeModal.jsx";
 const MainView = () => {
   const accessToken = sessionStorage.getItem("accessToken");
+  const [noticeID, setNoticeID] = useState("");
+
   const { status, data } = useQuery(["mainData", accessToken], async () => {
-    let res;
+    let res = [{}, {}];
     await axios
       .all([
         axios({
           method: "get",
-          url: BaseUrl + "/notice/list",
+          url: BaseUrl + "/notice/list/on",
           headers: {
             Authorization: `Bearer ${accessToken}`,
           },
@@ -41,11 +46,25 @@ const MainView = () => {
       ])
       .then(
         axios.spread((res1, res2) => {
-          console.log(res1);
-          res = [res1.data.content[0], res2.data.content[0]];
+          if (
+            res1.data.message === "프레임워크 내부적인 오류가 발생했습니다." ||
+            res2.data.message === "프레임워크 내부적인 오류가 발생했습니다."
+          ) {
+            res = [initialNotice, initialCompany];
+          } else {
+            if (res1.data.content[0] !== undefined)
+              res[0] = res1.data.content[0];
+            else res[0] = initialNotice;
+
+            if (res2.data.content[0] !== undefined)
+              res[1] = res2.data.content[0];
+            else res[1] = initialCompany;
+          }
         })
-      );
-    console.log(res);
+      )
+      .catch(() => {
+        res = [initialNotice, initialCompany];
+      });
     return res;
   });
 
@@ -61,26 +80,35 @@ const MainView = () => {
         </>
       ) : (
         <>
-          {" "}
           <Header />
           <Triangle_box>
             <Triangle />
           </Triangle_box>
           <Center>
             <Content>
-              <CA_View CA_data={data[0]} />
-              <Sub_Title
-                text="더 다양한 모집공고를 원하시나요?"
-                button="모집공고"
-              />
+              <CA_View CA_data={data[0]} detail={false} />
+              <div
+                style={{
+                  height: "500px",
+                  position: "relative",
+                  right: "-100px",
+                }}
+              >
+                <Sub_Title
+                  text="더 다양한 모집공고를 원하시나요?"
+                  button="모집공고"
+                />
+              </div>
             </Content>
             <Content width="937px" top="1533px" left="-170px">
-              <Sub_Title
-                text="여러 회사를 둘러보세요"
-                button="둘러보기"
-                color="#000"
-                top="50px"
-              />
+              <div style={{ height: "330px", position: "relative" }}>
+                <Sub_Title
+                  text="여러 회사를 둘러보세요"
+                  button="둘러보기"
+                  color="#000"
+                  top="50px"
+                />
+              </div>
               <IC_View IC_data={data[1]} />
             </Content>
           </Center>
@@ -94,12 +122,21 @@ const MainView = () => {
 export default MainView;
 
 const Sub_Title = ({ text, button, color, top }) => {
+  const navigate = useNavigate();
   return (
     <div>
       <Sub_Title_text color={color} top={top}>
         {text}
       </Sub_Title_text>
-      <Page_moving_btn width="200px">{button}</Page_moving_btn>
+      <Page_moving_btn
+        width="200px"
+        onClick={() => {
+          if (button === "모집공고") navigate("/student/notice");
+          else navigate("/student/company");
+        }}
+      >
+        {button}
+      </Page_moving_btn>
     </div>
   );
 };
@@ -132,6 +169,7 @@ const SubImg = styled.div`
 
 const Content = styled.div`
   position: absolute;
+  height: 100%;
   display: flex;
   width: ${(props) => props.width || "1100px"};
   top: ${(props) => props.top || "338px"};
@@ -141,7 +179,7 @@ const Content = styled.div`
 
 const Sub_Title_text = styled.span`
   display: block;
-  width: 336.5px;
+  width: 400px;
 
   font-family: NanumGothic;
   font-style: normal;
